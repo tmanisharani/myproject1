a0b7b48c97dd5e215e9f32445f90793840f4230c
Administrator <Administrator@PG0180BK>
2024-06-06 12:21:00 +0530

    employee update

--- src/com/edubridge/myproject1/model/Employee.java
+++ src/com/edubridge/myproject1/model/Employee.java
@@ -4,5 +4,23 @@ public class Employee {
 	private int employeeId;
 	private String employeeName;
 	private long employeeMobile;
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public long getEmployeeMobile() {
+		return employeeMobile;
+	}
+	public void setEmployeeMobile(long employeeMobile) {
+		this.employeeMobile = employeeMobile;
+	}
 
 }
